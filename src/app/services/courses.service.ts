09a24870e3fc786eaae607714398b8076a338d30
musa.nduati <musa.nduati@standardbank.co.za>
2024-11-01 01:48:51 +0200

    added a single data observable patter

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -5,42 +5,54 @@ import { Course } from "../model/course";
 import { map, shareReplay } from "rxjs/operators";
 import { Lesson } from "../model/lesson";
 
-@Injectable(
-    {providedIn: 'root'}
-)
+@Injectable({ providedIn: "root" })
 export class CoursesService {
+  constructor(private http: HttpClient) {}
 
-    constructor(
-        private http: HttpClient
-    ) {}
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course>("/api/courses").pipe(
+      map((res) => res["payload"]),
+      shareReplay()
+    );
+  }
 
-    
-    loadAllCourses(): Observable<Course[]> {
-        return this.http.get<Course>('/api/courses')
-        .pipe(
-            map(res => res["payload"]),
-            shareReplay()
-        );
-    }
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    return this.http
+      .put(`/api/courses/${courseId}`, changes)
+      .pipe(shareReplay());
+  }
 
-    saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
-        return this.http.put(`/api/courses/${courseId}`, changes)
-        .pipe(
-            shareReplay()
-        );  
-    }
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          filter: search,
+          pageSize: "100",
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
+  }
 
-    searchLessons(search: string): Observable<Lesson[]>{
-        return this.http.get<Lesson[]>('/api/lessons', {
-            params: {
-                filter: search,
-                pageSize: "100"
-            }
-        })
-        .pipe(
-            map(res => res["payload"]),
-            shareReplay()
-        );
-    }
+  loadCourseById(courseId: number): Observable<Course> {
+    return this.http
+      .get<Course>(`/api/courses/${courseId}`)
+      .pipe(shareReplay());
+  }
 
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          pageSize: "1000",
+          courseId: courseId.toString(),
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
+  }
 }
